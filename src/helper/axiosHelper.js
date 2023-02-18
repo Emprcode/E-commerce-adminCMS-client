2108f6b64fd6c1b3931c7797b222f2990a678b1f
Narayan Aryal <aryal.coding@gmail.com>
2023-02-18 14:03:39 +1100

    regirstration page and axios created

--- src/helper/axiosHelper.js
+++ src/helper/axiosHelper.js
@@ -1,20 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_ROOT_API
-const adminUrl = apiUrl + '/admin'
-
+const apiUrl = process.env.REACT_APP_ROOT_API;
+const adminUrl = apiUrl + "/admin";
 
 //register
 
-export const postAdminUser = async(userObj) => {
-    try {
-    const {data} = await axios.post(adminUrl, userObj)
-    console.log(data)
-    return data
-} catch (error) {
-    return{
-        status:'error',
-        message: error.message
-    }
-}
-}
+export const postAdminUser = async (userObj) => {
+  try {
+    const { data } = await axios.post(adminUrl, userObj);
+    console.log(data);
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
