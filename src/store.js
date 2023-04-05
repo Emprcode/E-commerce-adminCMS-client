c973d77a2505dae857298d5e4845799dc653ea01
Narayan Aryal <aryal.coding@gmail.com>
2023-04-05 12:27:40 +1000

    product reducer added in store.js

--- src/store.js
+++ src/store.js
@@ -4,12 +4,14 @@ import adminReducer from "./pages/admin-user/adminUserSlice";
 import systemReducer from "./system/systemSlice";
 import catReducer from "./pages/category/categorySlice";
 import paymentOptionsReducer from "./pages/payment-options/paymentOptionsSlice"
+import productReducer from "./pages/product/productSlice";
 const store = configureStore({
   reducer: {
     adminInfo: adminReducer,
     system: systemReducer,
     category: catReducer,
-    paymentOptions: paymentOptionsReducer
+    paymentOptions: paymentOptionsReducer,
+    product: productReducer 
   },
 });
 
