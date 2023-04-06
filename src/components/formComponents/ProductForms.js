ec36141397ca1cc8bab619cdecfba756a64862a5
Narayan Aryal <aryal.coding@gmail.com>
2023-04-06 13:02:13 +1000

    description added in product forms

--- src/components/formComponents/ProductForms.js
+++ src/components/formComponents/ProductForms.js
@@ -9,6 +9,7 @@ import { addNewProduct } from "../../pages/product/productAction";
 export const ProductForms = () => {
   const dispatch = useDispatch();
   const productRef = useRef("");
+  const descriptionRef = useRef()
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -21,12 +22,20 @@ export const ProductForms = () => {
     <div className="py-5 rounded shadow-lg p-3">
       <Form onSubmit={handleOnSubmit}>
         <Row className="m-3">
-          <Col className="col-8">
+          <Col className="col-3">
             <Form.Control
               placeholder="First name"
               ref={productRef}
               required
               name="name"
+            />
+          </Col>
+          <Col className="col-5">
+            <Form.Control
+              placeholder="Description"
+              ref={descriptionRef}
+              required
+              name="description"
             />
           </Col>
           <Col>
