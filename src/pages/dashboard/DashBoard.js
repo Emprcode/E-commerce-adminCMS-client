278b9743c57c53c524b9eea4ced02b169f968248
Narayan Aryal <aryal.coding@gmail.com>
2023-04-15 14:02:08 +1000

    Pie Chart added

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -39,69 +39,78 @@ const Dashboard = () => {
   });
   return (
     <Dashobardlayout>
-      <Container className="p-4">
-        <div className="d-flex justify-content-between">
-          <div className="fw-bold">
-            {" "}
-            <i className="fa-solid fa-house"></i> Dashboard
+      <div className="dashcolor">
+        <Container className="p-4">
+          <div className="d-flex justify-content-between">
+            <div className="fw-bold">
+              {" "}
+              <i className="fa-solid fa-house"></i> Dashboard
+            </div>
+            <div>
+              OverView <i className="fa-solid fa-circle-exclamation"></i>
+            </div>
           </div>
-          <div>
-            OverView <i className="fa-solid fa-circle-exclamation"></i>
-          </div>
-        </div>
-        <Row className=" p-4 gap-5 ">
-          <Col className="card1 p-3 rounded">
-            <div>
-              <div className="d-flex justify-content-around">
-                <p className="p-2">
-                  Weekly Sales <i class="fa-solid fa-chart-mixed"></i>
-                </p>
-              </div>
+          <Row className=" p-4 gap-5 ">
+            <Col className="card1 p-3 rounded">
+              <div>
+                <div className="d-flex justify-content-around">
+                  <p className="p-2">
+                    Weekly Sales <i class="fa-solid fa-chart-mixed"></i>
+                  </p>
+                </div>
 
-              <h4 className="fw-bold"> $ 50,000</h4>
-              <p className="p-3">Increased by 40%</p>
-            </div>
-          </Col>
-          <Col className="card2 p-3 rounded">
-            <div>
-              <div className="d-flex justify-content-around">
-                <p className="p-2">
-                  Weekly orders <i class="fa-solid fa-chart-mixed"></i>
-                </p>
+                <h4 className="fw-bold"> $ 50,000</h4>
+                <p className="p-3">Increased by 40%</p>
               </div>
+            </Col>
+            <Col className="card2 p-3 rounded">
+              <div>
+                <div className="d-flex justify-content-around">
+                  <p className="p-2">
+                    Weekly orders <i class="fa-solid fa-chart-mixed"></i>
+                  </p>
+                </div>
 
-              <h4 className="fw-bold">24,567</h4>
-              <p className="p-3">Decreased by 10%</p>
-            </div>
-          </Col>
-          <Col className="card3 p-3 rounded">
-            <div>
-              <div className="d-flex justify-content-around">
-                <p className="p-2">
-                  Visitors online <i class="fa-solid fa-chart-mixed"></i>
-                </p>
+                <h4 className="fw-bold">24,567</h4>
+                <p className="p-3">Decreased by 10%</p>
               </div>
+            </Col>
+            <Col className="card3 p-3 rounded">
+              <div>
+                <div className="d-flex justify-content-around">
+                  <p className="p-2">
+                    Visitors online <i class="fa-solid fa-chart-mixed"></i>
+                  </p>
+                </div>
 
-              <h4 className="fw-bold"> 98,895</h4>
-              <p className="p-3">Increased by 20%</p>
-            </div>
-          </Col>
-        </Row>
-        <Row className="p-4">
-          <Col>
-            <div>
-              <h4 className="fw-bold tect-center">
-                {" "}
-                Visit And Sales Statistics
-              </h4>
-            </div>
-            <BarChart chartData={userData} />
-          </Col>
-          <Col>
-            <PieChart />
-          </Col>
-        </Row>
-      </Container>
+                <h4 className="fw-bold"> 98,895</h4>
+                <p className="p-3">Increased by 20%</p>
+              </div>
+            </Col>
+          </Row>
+          <Row className=" mt-5 p-4 gap-5 ">
+            <Col className="bg-light rounded p-4">
+              <div>
+                <h6 className="fw-bold tect-center p-2">
+                  {" "}
+                  Visit And Sales Statistics
+                </h6>
+              </div>
+              <BarChart chartData={userData} />
+            </Col>
+            <Col className="bg-light text-center rounded p-4">
+              <div>
+                <h6 className="fw-bold tect-center p-2"> Traffic Sources</h6>
+              </div>
+              <div
+                style={{ width: 300, height: 350 }}
+                className="d-flex align-items-center mx-5">
+                <PieChart />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Dashobardlayout>
   );
 };
--- src/pages/dashboard/charts/PieCharts.js
+++ src/pages/dashboard/charts/PieCharts.js
@@ -3,41 +3,45 @@ import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 const PieChartData = [
-    
-    {
-      label: "traffic-source",
-      Australia: 40,
-      USA: 30,
-      Europe: 30,
-    },
-  ];
+  {
+    label: "Search Engines",
+    SearchEngines: 40,
+  },
+  {
+    label: "Direct Click",
+    DirectClick: 30,
+  },
+  {
+    label: "Bookmarks Click",
+    BookmarksClick: 30,
+  },
+];
 export const PieChart = () => {
-
-    const [userData, setUserData] = useState({
-        labels: PieChartData.map((data) => data.label),
-        datasets: [
-          {
-            label: "Australia",
-            data: PieChartData.map((data) => data.Australia),
-            backgroundColor: ["rgba(154, 85, 255, 1)"],
-            borderColor: "black",
-            borderWidth: 0,
-          },
-          {
-            label: "USA",
-            data: PieChartData.map((data) => data.USA),
-            backgroundColor: [ "rgba(254, 112, 150, 1)"],
-            borderColor: "black",
-            borderWidth: 0,
-          },
-          {
-            label: "Europe",
-            data: PieChartData.map((data) => data.Europe),
-            backgroundColor: ["rgba(54, 215, 232, 1)"],
-            borderColor: "black",
-            borderWidth: 0,
-          },
-        ]
-    });
+  const [userData, setUserData] = useState({
+    labels: PieChartData.map((data) => data.label),
+    datasets: [
+      {
+        label: "Search Engines",
+        data: PieChartData.map((data) => data.SearchEngines),
+        backgroundColor: ["rgba(154, 85, 255, 1)"],
+        borderColor: "black",
+        borderWidth: 0,
+      },
+      {
+        label: "Direct Click",
+        data: PieChartData.map((data) => data.DirectClick),
+        backgroundColor: ["rgba(254, 112, 150, 1)"],
+        borderColor: "black",
+        borderWidth: 0,
+      },
+      {
+        label: "Bookmarks Click",
+        data: PieChartData.map((data) => data.BookmarksClick),
+        backgroundColor: ["rgba(54, 215, 232, 1)"],
+        borderColor: "black",
+        borderWidth: 0,
+      },
+    ],
+  });
   return <Pie data={userData} />;
-}
+};
