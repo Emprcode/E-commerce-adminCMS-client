396c4cee4a6e2585f482aae222f96bda958b7594
Narayan Aryal <aryal.coding@gmail.com>
2023-04-26 11:59:15 +1000

    product delete method added

--- src/components/customTable/ProductTable.js
+++ src/components/customTable/ProductTable.js
@@ -1,72 +1,83 @@
-import React, { useEffect } from 'react'
-import { Button, Table, Form } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteSingleProductAction, getProductsAction } from '../../pages/product/productAction'
-
+import React, { useEffect } from "react";
+import { Button, Table, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteSingleProductAction,
+  getProductsAction,
+} from "../../pages/product/productAction";
 
 export const ProductTable = () => {
-
-  const { products } = useSelector((state) => state.product)
+  const { products } = useSelector((state) => state.product);
   // console.log(products)
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-useEffect(()=> {
-  dispatch(getProductsAction())
-}, [dispatch])
+  useEffect(() => {
+    dispatch(getProductsAction());
+  }, [dispatch]);
 
-
-const handleOnDelete = (_id) => {
-  if ( window.confirm("Are you sure you want to delete this item?")) {
-    
-    dispatch(deleteSingleProductAction({_id}))
-  }
-}
+  const handleOnDelete = (_id) => {
+    if (window.confirm("Are you sure you want to delete this item?")) {
+      dispatch(deleteSingleProductAction({ _id }));
+    }
+  };
   return (
     <div>
-        <select className='mb-2'>
-<option value=""> All</option>
-<option value=""> Active</option>
-<option value=""> Inactive</option>
-
+      <div className="d-flex justify-content-between mt-5">
+        <select className="mb-2">
+          <option value=""> All</option>
+          <option value=""> Active</option>
+          <option value=""> Inactive</option>
         </select>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Thumbnail</th>
-              <th>Status</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Description</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((item, i) => (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td>{
-                  <img src={"http://localhost:8000/" + item?.thumbnail.substr(6)} width="150px" alt='items'/>
-                  }</td>
+        <div className="fw-bold "> {products.length} items found!</div>
+      </div>
+      <hr />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Thumbnail</th>
+            <th>Status</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Description</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>
+                {
+                  <img
+                    src={"http://localhost:8000/" + item?.thumbnail.substr(6)}
+                    width="150px"
+                    alt="items"
+                  />
+                }
+              </td>
 
-                <td>
-                  <Form.Check type="switch" id="custom-switch" name="status" />
-                </td>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.qty}</td>
-                <td>{item.description}</td>
-                <td>
-                  <Button variant="warning">Edit</Button>{" "}
-                  <Button variant="danger" onClick={ ()=> handleOnDelete(item._id)}>Delete</Button>{" "}
-                 
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+              <td>
+                <Form.Check type="switch" id="custom-switch" name="status" />
+              </td>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>{item.qty}</td>
+              <td>{item.description}</td>
+              <td>
+                <Button variant="warning">Edit</Button>{" "}
+                <Button
+                  variant="danger"
+                  onClick={() => handleOnDelete(item._id)}>
+                  Delete
+                </Button>{" "}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
-  )
-}
+  );
+};
